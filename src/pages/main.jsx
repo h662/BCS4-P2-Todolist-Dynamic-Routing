1ca2b3977b2f015ab4885be8bce01b3aa8ff6f76
h662 <h662hong@gmail.com>
2023-11-13 07:43:43 +0900

    CreateTodo

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,24 +1,40 @@
+import { useEffect, useState } from "react";
+import CreateTodo from "../component/CreateTodo";
+
 const Main = () => {
+  const [todos, setTodos] = useState([]);
+  const [lastTodoId, setLastTodoId] = useState(0);
+
+  const getTodos = () => {
+    const localTodos = localStorage.getItem("todos");
+
+    if (!localTodos) return;
+
+    const parsedTodos = JSON.parse(localTodos);
+
+    setTodos(parsedTodos);
+    setLastTodoId(parsedTodos[parsedTodos.length - 1].id);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  useEffect(() => console.log(todos), [todos]);
+  useEffect(() => console.log(lastTodoId), [lastTodoId]);
+
   return (
     <div className="bg-red-100 min-h-screen">
       <main className="max-w-screen-md min-h-screen bg-yellow-100 mx-auto">
         <h1 className="bg-blue-100 py-12 text-center text-4xl font-bold">
           To do list
         </h1>
-        <form
-          className="bg-purple-100 w-96 mx-auto mt-12 flex"
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <input
-            className="w-3/4 mr-4 rounded-md p-2 focus:outline-none border-2 focus:border-blue-300"
-            type="text"
-          />
-          <input
-            className="w-1/4 bg-blue-400 hover:bg-blue-600 active:bg-blue-400 text-white font-semibold rounded-md"
-            type="submit"
-            value="생 성"
-          />
-        </form>
+        <CreateTodo
+          todos={todos}
+          setTodos={setTodos}
+          getTodos={getTodos}
+          lastTodoId={lastTodoId}
+        />
         <ul className="bg-purple-100 w-96 mx-auto h-[30rem] mt-12">
           <li className="bg-green-100 h-12 flex items-center text-xl">
             <span className="w-1/12 text-right bg-red-100">1</span>
