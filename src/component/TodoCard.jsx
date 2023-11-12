e5d826fe2831a11d1f2b2a4c27885a0d04155cbc
h662 <h662hong@gmail.com>
2023-11-13 08:21:47 +0900

    onClickIsDone

--- src/component/TodoCard.jsx
+++ src/component/TodoCard.jsx
@@ -4,7 +4,9 @@ const TodoCard = ({ todo }) => {
   return (
     <li className="h-12 flex items-center text-xl">
       <span className="w-1/12 text-right">{todo.id}</span>
-      <span className="w-8/12 pl-2">{todo.title}</span>
+      <span className={`w-8/12 pl-2 ${todo.isDone && "line-through"}`}>
+        {todo.title}
+      </span>
       <Link
         to={`/${todo.id}?title=${todo.title}&is-done=${todo.isDone}`}
         className="w-3/12 hover:font-bold"
--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,20 +1,46 @@
-import { useEffect } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams, useSearchParams, useNavigate } from "react-router-dom";
+import { FiCheck } from "react-icons/fi";
 
 const Detail = () => {
   const { id } = useParams();
   const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   const title = searchParams.get("title");
   const isDone = searchParams.get("is-done");
 
-  useEffect(() => console.log(id), [id]);
-  useEffect(() => console.log(title), [title]);
-  useEffect(() => console.log(isDone), [isDone]);
+  const onClickIsDone = () => {
+    const localTodos = localStorage.getItem("todos");
+
+    if (!localTodos) return;
+
+    const parsedTodos = JSON.parse(localTodos);
+
+    const updatedTodos = parsedTodos.map((v) => {
+      if (v.id == id) {
+        return { id: v.id, title: v.title, isDone: !v.isDone };
+      } else {
+        return v;
+      }
+    });
+
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+
+    navigate("/");
+  };
 
   return (
     <main className="max-w-screen-md min-h-screen mx-auto bg-red-100 flex justify-center items-center text-2xl">
-      Detail
+      <div className="flex items-center">
+        <span>{id}</span>
+        <span className="ml-4">{title}</span>
+        <button
+          onClick={onClickIsDone}
+          className="ml-4 bg-green-400 hover:bg-green-600 active:bg-green-400 rounded-md px-2 h-10 flex justify-center items-center"
+        >
+          <FiCheck /> {isDone ? " 완료" : " 진행중"}
+        </button>
+      </div>
     </main>
   );
 };
