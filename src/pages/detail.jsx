e9c62a7b110a4154852d79250f414a319d98053b
h662 <h662hong@gmail.com>
2023-11-13 08:28:10 +0900

    onClickDel

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,5 +1,5 @@
 import { useParams, useSearchParams, useNavigate } from "react-router-dom";
-import { FiCheck } from "react-icons/fi";
+import { FiCheck, FiTrash2 } from "react-icons/fi";
 
 const Detail = () => {
   const { id } = useParams();
@@ -29,6 +29,24 @@ const Detail = () => {
     navigate("/");
   };
 
+  const onClickDel = () => {
+    const localTodos = localStorage.getItem("todos");
+
+    if (!localTodos) return;
+
+    const parsedTodos = JSON.parse(localTodos);
+
+    const deletedTodos = parsedTodos.filter((v) => {
+      if (v.id !== +id) {
+        return v;
+      }
+    });
+
+    localStorage.setItem("todos", JSON.stringify(deletedTodos));
+
+    navigate("/");
+  };
+
   return (
     <main className="max-w-screen-md min-h-screen mx-auto bg-red-100 flex justify-center items-center text-2xl">
       <div className="flex items-center">
@@ -39,6 +57,12 @@ const Detail = () => {
           className="ml-4 bg-green-400 hover:bg-green-600 active:bg-green-400 rounded-md px-2 h-10 flex justify-center items-center"
         >
           <FiCheck /> {isDone ? " 완료" : " 진행중"}
+        </button>
+        <button
+          onClick={onClickDel}
+          className="ml-4 bg-red-400 hover:bg-red-600 active:bg-red-400 rounded-md px-2 h-10 flex justify-center items-center"
+        >
+          <FiTrash2 /> 삭제
         </button>
       </div>
     </main>
